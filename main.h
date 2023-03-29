8d1f152ec6dc29e26142d2da62738b8704ec619d
Ericka76 <erickacarmella8@gmail.com>
2023-03-29 07:34:13 -0700

    update main.h

--- main.h
+++ main.h
@@ -1,24 +1,28 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef _MAINH
+#define _MAINH
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
+#include <unistd.h>
 
 /**
- * Struct func-type-type structure
+ * Struct func-type-type of structure
  * @t: pointer to argument
  * @f: pointer - function associated with arguments
  */
-typedef struct func-type
+typedef struct func_type
 {
-	char *int _printf(const char *format, ...);t;
-	int (*f)(va_list);
+        chart;
+        int (f) (va_list);
 }func_t;
+int (get_func(const char format))(va_list);
 int _putchar(char c);
-int _printf(const char *format, ...);
-int (* get_func(const char * format))(va_list);
+int _printf(const charformat, ...);
+int print_str(va_list args);
+int print_char(va_list args);
+int print_pct(va_list args);
+int print_dec(va_list args)
 
 #endif
-
