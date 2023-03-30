ec58b4391b3390c72238e7781c14e7d43aeb0858
Alliahsam <alliah2@hotmail.fr>
2023-03-30 07:17:35 -0700

    modif du functions.c

--- functions.c
+++ functions.c
@@ -1,11 +1,11 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- *print_char - prints character
- *@args: argument
- *
- *Return: character
- */
+* print_char - prints character
+*@args: argument
+*
+*Return: character
+*/
 
 int print_char(va_list args)
 {
@@ -14,15 +14,17 @@ int print_char(va_list args)
 }
 
 /**
- *print_str - prints string
- *@args: arguments
- *Return: string
- */
-int print_str(va_list args)
+* print_str - prints string
+* @args: arguments
+* Return: string
+*/
+
+int print_string(va_list args)
 {
         int j;
-        char str = va_arg(args, char *);
-        if (str == NULL)
+        char *str = va_arg(args, char *);
+
+        if ( str == NULL)
         str = "(null)";
         for (j = 0; str[j]; j++)
         {
@@ -32,13 +34,11 @@ int print_str(va_list args)
 }
 
 /**
- *print_pct - prints character "%"
- *@args : arguments
- *
- *Return: character "%"
- */
-
-int print_pct(va_list args)
+* print_pct - prints character "%"
+* @args : arguments
+* Return: character "%"
+*/
+int print_pctent(va_list args)
 {
         (void)args;
         return (write(1, "%", 1));
