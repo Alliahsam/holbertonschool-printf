f506713223269f5d7c4047ded9ed519f6cb79f8b
Alliahsam <alliah2@hotmail.fr>
2023-03-30 07:12:16 -0700

    modification de get_func.c

--- get_func.c
+++ get_func.c
@@ -1,25 +1,26 @@
 #include "main.h"
 
 /**
- * get_func - check for valid specifier
- * @format: a character to check
- * Return: a pointer to the function
+* get_func - check for valid specifier
+* @format: a character to check
+* Return: a pointer to the function
 */
-int (*get_func(const char format))(va_list)
+
+int (*get_func(const char *format))(va_list)
 {
         int i;
         func_t p[] = {
         {"c", print_char},
-        {"s", print_str},
-        {"%", print_pct},
-        {"d", print_dec},
-        {"i", print_dec},
+        {"s", print_string},
+        {"%", print_pctent},
+        {"d", print_decimal},
+        {"i", print_decimal},
         {NULL, NULL}
         };
 
         for (i = 0; p[i].t; i++)
         {
-        if (format == *(p[i].t))
+        if (*format == *(p[i].t))
         {
         return (p[i].f);
         }
