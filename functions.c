0a9e5042905a776609becd8bc0aa3527709f7e6e
Alliahsam <alliah2@hotmail.fr>
2023-03-31 02:57:43 -0700

    update files

--- functions.c
+++ functions.c
@@ -1,45 +1,46 @@
 #include "main.h"
 
 /**
-* print_char - prints character
-*@args: argument
-*
-*Return: character
-*/
+ * print_char - prints character
+ *@args: argument
+ *
+ *Return: character
+ */
 
 int print_char(va_list args)
 {
-        _putchar(va_arg(args, int));
-        return (1);
+_putchar(va_arg(args, int));
+return (1);
 }
 
 /**
-* print_str - prints string
-* @args: arguments
-* Return: string
-*/
+ * print_str - prints string
+ * @args: arguments
+ * Return: string
+ */
 
 int print_string(va_list args)
 {
-        int j;
-        char *str = va_arg(args, char *);
+int j;
+char *str = va_arg(args, char *);
 
-        if ( str == NULL)
-        str = "(null)";
-        for (j = 0; str[j]; j++)
-        {
-        _putchar(str[j]);
-        }
-        return (j);
+if (str == NULL)
+str = "(null)";
+for (j = 0; str[j]; j++)
+{
+_putchar(str[j]);
+}
+return (j);
 }
 
 /**
-* print_pct - prints character "%"
-* @args : arguments
-* Return: character "%"
-*/
+ * print_pct - prints character "%"
+ * @args : arguments
+ * Return: character "%"
+ */
+
 int print_pctent(va_list args)
 {
-        (void)args;
-        return (write(1, "%", 1));
+(void)args;
+return (write(1, "%", 1));
 }
